38629c7d69b8144faa550331700779851c33733e
O-GAMES <79658234+O-GAMES@users.noreply.github.com>
2021-07-27 07:20:23 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "hello frind - مرحباَ صديق"
+
+echo "
+
+
+Copying the necessary files - جاري نسخ الملفات الضرورية"
+
+cp config_i3/*  $HOME/.config/i3
+
+echo "
+
+
+
+
+We're done installing the files , Have fun my friend. -  لقد انتهينا من تثبيت الملفات , استمتع يا صديقي 
+
+
+
+
+
+Restart your device  - إعادة تشغيل جهازك
+
+
+"
+
